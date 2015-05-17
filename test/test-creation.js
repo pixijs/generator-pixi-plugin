a1e96c46d560312eb4754de5b332ff6a97907e8b
Chad Engler <englercj@live.com>
2015-05-17 10:08:45 -0700

    fix tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -8,7 +8,7 @@ describe('browserify module generator', function () {
                 return done(err);
             }
 
-            this.app = helpers.createGenerator('browserify:app', ['../../generators/app']);
+            this.app = helpers.createGenerator('pixi-plugin:app', ['../../generators/app']);
             done();
         }.bind(this));
     });
@@ -28,19 +28,14 @@ describe('browserify module generator', function () {
             'gulp/tasks/build.js',
             'gulp/tasks/clean.js',
             'gulp/tasks/dev.js',
-            'gulp/tasks/jsdoc.js',
             'gulp/tasks/jshint.js',
             'gulp/tasks/scripts.js',
-            'gulp/tasks/test.js',
             'gulp/tasks/watch.js',
             'gulp/util/bundle.js',
             'gulp/util/handleErrors.js',
             'gulp/util/jsdoc.conf.json',
             // src files
-            'src/index.js',
-            // test files
-            'test/fixtures/.gitkeep',
-            'test/unit/index.test.js'
+            'src/index.js'
         ];
 
         helpers.mockPrompt(this.app, {
